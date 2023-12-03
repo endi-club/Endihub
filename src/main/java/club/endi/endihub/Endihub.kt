8d367ba9626e23b8b2714b78b40c79647d978802
Perny <pernydev@gmail.com>
2023-12-03 23:43:59 +0200

    Made it tp you to spawn when you join

--- src/main/java/club/endi/endihub/Endihub.kt
+++ src/main/java/club/endi/endihub/Endihub.kt
@@ -1,19 +1,27 @@
 package club.endi.endihub
 
+import org.bukkit.Location
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockFormEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class Endihub : JavaPlugin(), Listener {
     override fun onEnable() {
-        // Listen for events
+        saveDefaultConfig()
         server.pluginManager.registerEvents(this, this)
     }
 
     @EventHandler
     fun onBlockFormEvent(event: BlockFormEvent) {
         event.isCancelled = true
+    }
+
+    @EventHandler
+    fun onPlayerJoinEvent(event: PlayerJoinEvent) {
+        event.player.teleport(event.player.world.spawnLocation)
     }
 
     override fun onDisable() {
