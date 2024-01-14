cb1b11a271766a4fa8212ebef5d7b108ca63add7
Perny <pernydev@gmail.com>
2024-01-14 17:31:29 +0200

    EEEEEE

--- src/main/java/club/endi/endihub/Endihub.kt
+++ src/main/java/club/endi/endihub/Endihub.kt
@@ -1,9 +1,11 @@
 package club.endi.endihub
 
+import club.endi.endihub.util.Text
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Location
 import org.bukkit.Material
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
@@ -11,8 +13,8 @@ import org.bukkit.event.block.BlockFormEvent
 import org.bukkit.event.block.BlockIgniteEvent
 import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.entity.EntityDamageEvent
-import org.bukkit.event.player.PlayerJoinEvent
-import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.player.*
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -34,10 +36,15 @@ class Endihub : JavaPlugin(), Listener {
         event.player.inventory.clear()
 
         val compassItem = ItemStack(Material.DIRT, 1)
-        compassItem.lore(List(1) { Component.text("Vasen klikkaus: Avaa pelaajan valikko").color(NamedTextColor.GRAY) })
+        compassItem.lore(List(1) { Text.md("&e**ᴘʀɪɢʜᴛᴄʟɪᴄᴋ**: Avaa pelaajan valikko").color(NamedTextColor.GRAY) })
 
         event.player.inventory.setItem(1, compassItem)
-        event.player.inventory.setItem(4, compassItem)
+
+        val cosmeticsItem = ItemStack(Material.CHEST, 1)
+        cosmeticsItem.lore(List(1) { Component.text("&e**ᴘʀɪɢʜᴛᴄʟɪᴄᴋ**: Avaa kosmeettikka valikko").color(NamedTextColor.GRAY) })
+
+        event.player.inventory.setItem(4, cosmeticsItem)
+
         event.player.inventory.setItem(7, compassItem)
     }
 
@@ -81,7 +88,34 @@ class Endihub : JavaPlugin(), Listener {
         event.isCancelled = !event.player.hasPermission("endihub.build")
     }
 
+    @EventHandler
+    fun onPlayerDropItemEvent(event: PlayerDropItemEvent) {
+        event.isCancelled = true
+    }
+
+    @EventHandler
+    fun onPlayerSwapHandItemsEvent(event: PlayerSwapHandItemsEvent) {
+        event.isCancelled = true
+    }
+
+    // rightclick handler
+    @EventHandler
+    fun onPlayerInteractEvent(event: PlayerInteractEvent) {
+        if (event.item?.type == Material.CHEST) {
+            event.player.performCommand("kosmeettikka")
+        }
+    }
+
+    // disallow item dragging
+    @EventHandler
+    fun onInventoryClickEvent(event: InventoryClickEvent) {
+        if (event.inventory.holder is Player) {
+            event.isCancelled = true
+        }
+    }
+
     override fun onDisable() {
         // Plugin shutdown logic
     }
+
 }
---
+++ src/main/java/club/endi/endihub/util/Text.kt
@@ -0,0 +1,76 @@
+package club.endi.endihub.util
+
+import de.themoep.minedown.adventure.MineDown
+import net.kyori.adventure.text.Component
+
+val colors = mapOf(
+    "&(primary-1)" to "&#913AFF&",
+    "&(primary-2)" to "&#FF3AEB&",
+    "&(primary-3)" to "&#945DDB&",
+    "&(primary-gradient)" to "&#913AFF-#FF3AEB&",
+
+    "&(energy)" to "&#FF9900&",
+    "&(time)" to "&#00FFA3&",
+
+    "&(gray-1)" to "&#AAAAAA&",
+    "&(gray-2)" to "&#555555&",
+
+    "&(err)" to "&#FF3D00&",
+    "&(success)" to "&#8FFF00&",
+    "&(warn)" to "&#FFE500&",
+)
+
+class Text {
+    companion object {
+        const val prefix = "**&#913AFF-#FF3AEB&Endi »**&f "
+        private fun parser(input: String): String {
+            var output = input
+            for (color in colors) {
+                output = output.replace(color.key, color.value)
+            }
+            return output
+        }
+        fun md(input: String): Component {
+            return MineDown(parser(input)).toComponent()
+        }
+
+        fun mdUntrusted(input: String, untrustedString: String): Component {
+            return MineDown(parser(input)).replace("<untrusted>", untrustedString).toComponent()
+        }
+
+        fun pre(input: String): Component {
+            return MineDown(prefix + parser(input)).toComponent()
+        }
+
+        fun breakText(input: String, maxCharactersPerLine: Int): List<String> {
+            val words = input.split(" ")
+            val lines = mutableListOf<String>()
+
+            var currentLine = ""
+            for (word in words) {
+                currentLine = if (currentLine.isEmpty()) {
+                    word
+                } else {
+                    val potentialLine = "$currentLine $word"
+                    if (potentialLine.length <= maxCharactersPerLine) {
+                        potentialLine
+                    } else {
+                        lines.add(currentLine)
+                        word
+                    }
+                }
+            }
+
+            if (currentLine.isNotEmpty()) {
+                lines.add(currentLine)
+            }
+
+            return lines
+        }
+
+        fun mdlegacy(input: String): String {
+            val component = MineDown(parser(input)).toComponent()
+            return net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer.legacySection().serialize(component)
+        }
+    }
+}
